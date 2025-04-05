d9f1cd6e55439ce52921d0126a54efbf141c949e
pablo-cunha <lcunha.pablo@gmail.com>
2025-04-05 15:51:26 -0300

    Implementa dominio da aplicação

---
+++ src/main/java/br/com/pablocunha/todolist/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package br.com.pablocunha.todolist.repository;
+
+import br.com.pablocunha.todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
